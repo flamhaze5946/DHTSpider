14f50ec68149da45035f328e77a4b3e22b153b76
Flamhaze <flamhaze@FlamhazedeMacBook-Pro.local>
2017-04-05 16:28:50 +0800

    添加了一个成功的测试用例

--- src/test/java/com/zx/dhtspider/test/SpiderUtilTest.java
+++ src/test/java/com/zx/dhtspider/test/SpiderUtilTest.java
@@ -30,4 +30,15 @@ public class SpiderUtilTest
         String result = "0c1741";
         Assert.assertEquals(SpiderUtils.bytesToHexString(src), result, "字节转化结果不匹配");
     }
+
+    /**
+     * 成功的Long数字转ip地址测试
+     */
+    @Test
+    public void successLong2IpAdressTest()
+    {
+        long src = 2312123213L;
+        String result = "137.208.51.77";
+        Assert.assertEquals(SpiderUtils.long2IpAdress(src), result);
+    }
 }
